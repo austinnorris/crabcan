21da68df3ef7a88f2f514f003c9264598e4aafde
Austin Norris <austinnorris@ucla.edu>
2022-10-13 20:24:08 -0700

    Removes old code from tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,3 @@
-// use std::fs;
-
 use assert_cmd::Command;
 use predicates::prelude::*;
 
@@ -13,34 +11,3 @@ fn no_arguments() -> TestResult {
         .stderr(predicate::str::contains("USAGE"));
     Ok(())
 }
-
-// fn run(args: &[&str], expected_file: &str) -> TestResult {
-//     let expected = fs::read_to_string(expected_file)?;
-//     Command::cargo_bin("echor")?
-//         .args(args)
-//         .assert()
-//         .success()
-//         .stdout(expected);
-//     Ok(())
-// }
-
-
-// #[test]
-// fn hello1() -> TestResult {
-//     run(&["Hello there"], "tests/expected/hello1.txt")
-// }
-
-// #[test]
-// fn hello2() -> TestResult {
-//     run(&["Hello", "there"], "tests/expected/hello2.txt")
-// }
-
-// #[test]
-// fn hello1_no_newline() -> TestResult {
-//     run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
-// }
-
-// #[test]
-// fn hello2_no_newline() -> TestResult {
-//     run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
-// }
