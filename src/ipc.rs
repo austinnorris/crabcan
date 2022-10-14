544281b554ef6f1d7eb348af160d23601a9f63ef
Austin Norris <austinnorris@ucla.edu>
2022-10-13 20:49:01 -0700

    Wrappers to send bool value over socket

--- src/ipc.rs
+++ src/ipc.rs
@@ -14,3 +14,21 @@ pub fn generate_socket_pair() -> Result<(RawFd, RawFd), ErrCode> {
         Err(_) => Err(ErrCode::SocketError(0)),
     }
 }
+
+pub fn send_boolean(fd: RawFd, msg: bool) -> Result<(), ErrCode> {
+    let data: [u8; 1] = [msg.into()];
+    if let Err(e) = send(fd, &data, MsgFlags::empty()) {
+        log::error!("Cannot send boolean through socket: {:?}", e);
+        return Err(ErrCode::SocketError(1));
+    };
+    Ok(())
+}
+
+pub fn recv_boolean(fd: RawFd) -> Result<bool, ErrCode> {
+    let mut data: [u8; 1] = [0];
+    if let Err(e) = recv(fd, &mut data, MsgFlags::empty()) {
+        log::error!("Cannot receive boolean from socket: {:?}", e);
+        return Err(ErrCode::SocketError(2));
+    }
+    Ok(data[0] == 1)
+}
